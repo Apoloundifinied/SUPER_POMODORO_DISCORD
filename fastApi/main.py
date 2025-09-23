23caf0be2e74d37f6c06150df4b33f8dec1fb954
ApoloUndifinied <empreendendo019@gmail.com>
2025-09-22 21:29:39 -0300

    issues resolvidos e frases

--- fastApi/main.py
+++ fastApi/main.py
@@ -3,21 +3,26 @@ import random
 import json
 app = FastAPI()
 
+
+
 with open("frases.json", "r", encoding="utf-8") as f:
     data = json.load(f)
     frases = data["frases"]
+
+
 
 @app.get("/")
 def read_root():
     return {"mensagem": "API de Frases Motivacionais"}
 
-# 🔹 Rota que retorna uma frase aleatória
+
 @app.get("/frases")
 def get_frase():
     frase_aleatoria = random.choice(frases)
     return {"frase": frase_aleatoria}
 
-# 🔹 Rota com quantidade de frases aleatórias (opcional)
+
 @app.get("/frases/{quantidade}")
 def get_varias_frases(quantidade: int):
     return {"frases": random.sample(frases, min(quantidade, len(frases)))}
+
